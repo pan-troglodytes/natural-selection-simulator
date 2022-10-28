d3a8f6d920956699871b58cd29ac8a5e9beac6f2
sacha <pantroggy@protonmail.com>
2022-10-28 12:54:51 +0000

    fixed rabbit births

--- rabbits.gd
+++ rabbits.gd
@@ -9,8 +9,9 @@ func birth(parents, mother):
 	var rabbitNew = load("res://rabbit.tscn").instance()
 	
 	add_child(rabbitNew)
-	
+	rabbitNew.setTraits(parents, true)
 	rabbitNew.global_transform.origin = mother.get_node("babySpawner").global_transform.origin
 	rabbitNew.energy = mother.traits["reproductiveCost"]
 	rabbitNew.name = "rabbit" + str(rabbitsQuantity)
+
 	
